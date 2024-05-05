f60eda1e7d401e9a4fbea6d25c6b8cbf5218ad3b
Victorivanov <69059387+VictorivanovYT@users.noreply.github.com>
2024-05-05 21:55:49 +0300

    Fixed Reading Part 2

--- main.js
+++ main.js
@@ -413,160 +413,159 @@ function b1level(){
             ans.innerHTML = `1.${n1}, 2.${n2}, 3.${n3}, 4.${n4}, 5.${n5}, 6.${n6}, 7.${n7}, 8.${n8}`
             break
         case '2.2': //MISSED PART PLEASE REDO!!!
-            /*switch (task){
+            switch (task){
                 case '1':
-                        n1="C"
-                        n2="A"
-                        n3="B"
-                        n4="A"
-                        n5="C"
-                        n6="A"
-                        n7="C"
-                        n8="B"
-                        break
-                    case '2':
                         n1="B"
                         n2="C"
-                        n3="A"
-                        n4="C"
+                        n3="D"
+                        n4="B"
                         n5="A"
                         n6="B"
                         n7="C"
-                        n8="A"
+                        n8="D"
+                        break
+                    case '2':
+                        n1="C"
+                        n2="C"
+                        n3="C"
+                        n4="D"
+                        n5="B"
+                        n6="D"
+                        n7="B"
+                        n8="D"
                         break
                     case '3':
                         n1="B"
-                        n2="A"
-                        n3="B"
-                        n4="A"
+                        n2="D"
+                        n3="C"
+                        n4="C"
                         n5="C"
-                        n6="A"
-                        n7="B"
-                        n8="B"
+                        n6="D"
+                        n7="C"
+                        n8="C"
                     break
                     case '4':
                         n1="B"
-                        n2="B"
-                        n3="A"
-                        n4="A"
-                        n5="C"
-                        n6="B"
-                        n7="C"
-                        n8="A"
+                        n2="A"
+                        n3="C"
+                        n4="B"
+                        n5="B"
+                        n6="D"
+                        n7="D"
+                        n8="B"
                     break
                     case '5':
-                        n1="A"
-                        n2="C"
+                        n1="C"
+                        n2="A"
                         n3="C"
                         n4="B"
                         n5="C"
                         n6="C"
-                        n7="A"
-                        n8="A"
+                        n7="D"
+                        n8="B"
                     break
                     case '6':
                         n1="C"
                         n2="A"
-                        n3="C"
-                        n4="B"
-                        n5="B"
+                        n3="D"
+                        n4="C"
+                        n5="C"
                         n6="A"
-                        n7="C"
-                        n8="C"
+                        n7="B"
+                        n8="D"
                     break
                     case '7':
-                        n1="B"
-                        n2="C"
-                        n3="A"
-                        n4="B"
-                        n5="B"
-                        n6="C"
-                        n7="A"
-                        n8="C"
+                        n1="C"
+                        n2="A"
+                        n3="D"
+                        n4="C"
+                        n5="A"
+                        n6="B"
+                        n7="B"
+                        n8="D"
                     break
                     case '8':
                         n1="C"
-                        n2="A"
-                        n3="B"
-                        n4="A"
-                        n5="A"
+                        n2="B"
+                        n3="C"
+                        n4="B"
+                        n5="D"
                         n6="C"
                         n7="A"
-                        n8="B"
-                    break
-                    case '9':
-                        n1="B"
-                        n2="B"
-                        n3="C"
-                        n4="A"
-                        n5="B"
-                        n6="A"
-                        n7="C"
                         n8="C"
                     break
-                    case '10':
+                    case '9':
                         n1="A"
                         n2="B"
-                        n3="C"
-                        n4="A"
-                        n5="C"
-                        n6="B"
-                        n7="C"
-                        n8="B"
-                    break
-                    case '11':
-                        n1="A"
-                        n2="C"
-                        n3="B"
+                        n3="D"
                         n4="A"
                         n5="B"
                         n6="A"
                         n7="C"
+                        n8="D"
+                    break
+                    case '10':
+                        n1="C"
+                        n2="B"
+                        n3="A"
+                        n4="C"
+                        n5="C"
+                        n6="D"
+                        n7="A"
+                        n8="D"
+                    break
+                    case '11':
+                        n1="B"
+                        n2="D"
+                        n3="A"
+                        n4="B"
+                        n5="D"
+                        n6="B"
+                        n7="D"
                         n8="B"
                     break
                     case '12':
                         n1="C"
-                        n2="A"
-                        n3="B"
-                        n4="A"
-                        n5="B"
-                        n6="C"
+                        n2="D"
+                        n3="D"
+                        n4="B"
+                        n5="C"
+                        n6="D"
                         n7="A"
                         n8="C"
                     break
                     case '13':
-                        n1="C"
-                        n2="A"
-                        n3="B"
+                        n1="B"
+                        n2="C"
+                        n3="D"
                         n4="C"
                         n5="A"
-                        n6="A"
-                        n7="C"
+                        n6="B"
+                        n7="A"
                         n8="B"
                     break
                     case '14':
                         n1="A"
                         n2="C"
                         n3="A"
-                        n4="B"
-                        n5="C"
-                        n6="A"
+                        n4="C"
+                        n5="A"
+                        n6="D"
                         n7="A"
                         n8="C"
                     break
                     case '15':
-                        n1="A"
+                        n1="D"
                         n2="C"
                         n3="B"
-                        n4="C"
-                        n5="A"
+                        n4="A"
+                        n5="B"
                         n6="C"
                         n7="A"
-                        n8="B"
+                        n8="C"
                     break
                 }
-                ans.innerHTML = `1.${n1}, 2.${n2}, 3.${n3}, 4.${n4}, 5.${n5}, 6.${n6}, 7.${n7}, 8.${n8}`*/
-                ans.innerHTML = "THIS PART IS CURRENTLY INAVAILABLE BECAUSE OF AN ANSWER INACCURACY, IT WILL BE FIXED AT A LATER TIME"
+                ans.innerHTML = `1.${n1}, 2.${n2}, 3.${n3}, 4.${n4}, 5.${n5}, 6.${n6}, 7.${n7}, 8.${n8}`
                 break
         case '1.3':
             switch (task){
